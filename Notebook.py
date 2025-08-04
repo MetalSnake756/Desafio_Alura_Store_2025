2a2cf0e6cc430ab0387d61eb0ed4a773e1180623
MetalSnake756 <mcjesus.756@gmail.com6>
2025-08-03 22:05:29 -0400

    03-08

--- Notebook.py
+++ Notebook.py
@@ -23,19 +23,24 @@ df = {}
 for name, url in urls.items():
     df[name] = pd.read_csv(url)
 
+
 # Acceso: dataframes['st1'], dataframes['st2'], etc.
 
 sum_prec = []
+vent_cat = []
+cal_cat = []
+env_cat = []
 
 for i in range (1,5):
     sum_prec.append((df[f'{i}'].Precio.sum()))
-    ganancias = pd.DataFrame({'Ganancia': sum_prec})
+    vent_cat.append((df[f'{i}']['Categoría del Producto'].value_counts()))
+    cal_cat.append((df[f'{i}'].Calificación.mean().round(2)))
+    env_cat.append((df[f'{i}']['Costo de envío'].mean().round(2)))
 
-ganancias.rename(index={0: "Tienda 1",
-                        1: "Tienda 2",
-                        2: "Tienda 3",
-                        3: "Tienda 4"}, inplace=T )
 
+
+ganancias = pd.DataFrame({'Ganancia': sum_prec})
+ganancias.index = [f'Tienda {i}' for i in range(1, 5)]
 ganancias.rename_axis("Tienda", inplace = T) 
 
 sns.set_style("whitegrid") # Set style for chart
@@ -48,10 +53,10 @@ for i in range(len(ganancias.Ganancia)):
         plt.text(i-0.35, ganancias.Ganancia[i], ganancias.Ganancia[i])  # Placing text slightly above the bar
 
 bx = ganancias.plot(kind="bar", 
-               title='Ganacias de Cada Tienda', 
-               xlabel='Tienda Individual',
-               ylabel='Porcentaje',
-               legend=F)
+                title='Ganacias de Cada Tienda', 
+                xlabel='Tienda Individual',
+                ylabel='Porcentaje',
+                legend=F)
 bx.ticklabel_format(style='plain', axis='y')
 
 
@@ -62,3 +67,43 @@ plt.pie(ganancias.Ganancia,
         explode = [0.1, 0 , 0, 0],
         labels = ganancias.index,
         autopct ='%.2f%%')
+
+ventas = pd.concat(vent_cat, axis=1)
+ventas.columns = [f'Tienda {i}' for i in range(1, 5)]
+ventas
+
+vent = ventas.plot(kind='barh', 
+                   rot=0, 
+                   xlabel = 'Unidades Vendidas',
+                   title = 'Ventas por Categorias')
+vent.invert_yaxis()
+
+cal_env = pd.DataFrame({"Calificacion Promedio": cal_cat,
+                        'Promedio envio':env_cat}, 
+                       index= [f'Tienda {i}' for i in range(1, 5)])
+cal_env.rename_axis("Tienda", inplace = T) 
+
+cal_env.reset_index().plot(kind='scatter',
+                                x='Tienda',
+                                y="Calificacion Promedio",
+                                marker='^',
+                                color='green',
+                                s=150)
+
+ventas.transpose().apply(lambda x: x*100/sum(x), axis=1).plot(kind="barh", stacked=True)
+plt.title("Porcentaje de productos vendidos")
+plt.ylabel("Tienda")
+plt.xlabel("Ventas (%)")
+plt.legend(
+    title="Categoría del Producto",
+    bbox_to_anchor=(1, 0.75),   # Posición: ligeramente a la derecha
+    loc='upper left')            # Ubicación relativa del ancla)
+plt.show()
+
+ex = sns.barplot(cal_env.reset_index(), 
+                x = 'Tienda',
+                y='Promedio envio',
+                palette="hls",
+                errorbar=None)
+for container in ex.containers:
+        ex.bar_label(container, fmt='%.2f') # Format labels to two decimal places
